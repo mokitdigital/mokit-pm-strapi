fd11a3ac55657b7cedbe048150972bb1bf780d97
Luan Silveira <luan.silveira@zrp.com.br>
2024-10-29 02:03:15 -0300

    ajustado webhook para status de pagamento

--- src/api/payment/controllers/payment.ts
+++ src/api/payment/controllers/payment.ts
@@ -2,78 +2,101 @@
  * payment controller
  */
 
-import { factories } from '@strapi/strapi'
-import { CreateTransactionAsaasSchema } from '../validations/schema';
+import { factories } from "@strapi/strapi";
+import { CreateTransactionAsaasSchema } from "../validations/schema";
 
 enum EventWebhookAsaas {
-  PAYMENT_CREATED = 'PAYMENT_CREATED',
-  PAYMENT_CONFIRMED = 'PAYMENT_CONFIRMED',
-  PAYMENT_CHECKOUT_VIEWED = 'PAYMENT_CHECKOUT_VIEWED',
-  PAYMENT_RECEIVED = 'PAYMENT_RECEIVED',
+  PAYMENT_CREATED = "PAYMENT_CREATED",
+  PAYMENT_CONFIRMED = "PAYMENT_CONFIRMED",
+  PAYMENT_CHECKOUT_VIEWED = "PAYMENT_CHECKOUT_VIEWED",
+  PAYMENT_RECEIVED = "PAYMENT_RECEIVED",
 }
 
 type WebhookAsaasPayload = {
-  id: string,
-  event: EventWebhookAsaas,
-  dateCreated: string,
+  id: string;
+  event: EventWebhookAsaas;
+  dateCreated: string;
   payment: {
-    object: string,
-    id: string,
-    dateCreated: string,
-    customer: string,
-    paymentLink: string,
-    value: number,
-    netValue: number,
-    description: "Qualquer produto em até 10x de R$ 50,00",
-    billingType: "PIX",
-    pixTransaction: any,
-    status: "PENDING" | "CONFIRMED" | any,
-    dueDate: string,
-    originalDueDate: string,
-    paymentDate: string,
-    clientPaymentDate: string,
-    installmentNumber: number,
-    invoiceUrl: string,
-    invoiceNumber: string,
-    externalReference: string,
-    deleted: boolean,
-    anticipated: boolean,
-    anticipable: boolean,
-    creditDate: string,
-    estimatedCreditDate: string,
-    transactionReceiptUrl: string,
-    nossoNumero: number,
-    bankSlipUrl: string,
-    lastInvoiceViewedDate: any,
-    lastBankSlipViewedDate: any,
-    postalService: boolean,
-    custody: any,
-    refunds: any
-  }
-}
+    object: string;
+    id: string;
+    dateCreated: string;
+    customer: string;
+    paymentLink: string;
+    value: number;
+    netValue: number;
+    description: "Qualquer produto em até 10x de R$ 50,00";
+    billingType: "PIX";
+    pixTransaction: any;
+    status: "PENDING" | "CONFIRMED" | any;
+    dueDate: string;
+    originalDueDate: string;
+    paymentDate: string;
+    clientPaymentDate: string;
+    installmentNumber: number;
+    invoiceUrl: string;
+    invoiceNumber: string;
+    externalReference: string;
+    deleted: boolean;
+    anticipated: boolean;
+    anticipable: boolean;
+    creditDate: string;
+    estimatedCreditDate: string;
+    transactionReceiptUrl: string;
+    nossoNumero: number;
+    bankSlipUrl: string;
+    lastInvoiceViewedDate: any;
+    lastBankSlipViewedDate: any;
+    postalService: boolean;
+    custody: any;
+    refunds: any;
+  };
+};
 
-export default factories.createCoreController('api::payment.payment', ({ strapi }) => ({
-  async createTransactionAsaas(ctx) {
-    const { data } = ctx.request.body;
+export default factories.createCoreController(
+  "api::payment.payment",
+  ({ strapi }) => ({
+    async createTransactionAsaas(ctx) {
+      const { data } = ctx.request.body;
 
-    const paymentSchema = await CreateTransactionAsaasSchema.validate(data);
+      const paymentSchema = await CreateTransactionAsaasSchema.validate(data);
 
-    const payment = await strapi.service('api::payment.payment').createTransactionAsaas(paymentSchema);
-    return payment;
-  },
-  async webhook(ctx) {
-    const response: WebhookAsaasPayload = ctx.request.body;
+      const payment = await strapi
+        .service("api::payment.payment")
+        .createTransactionAsaas(paymentSchema);
+      return payment;
+    },
+    async webhook(ctx) {
+      const response: WebhookAsaasPayload = ctx.request.body;
 
-    if (response.event === EventWebhookAsaas.PAYMENT_CREATED) {
-      console.log("PAYMENT_CREATED", response);
-    } else if (response.event === EventWebhookAsaas.PAYMENT_CONFIRMED) {
-      console.log("PAYMENT_CONFIRMED", response);
-    } else if (response.event === EventWebhookAsaas.PAYMENT_CHECKOUT_VIEWED) {
-      console.log("PAYMENT_CHECKOUT_VIEWED", response);
-    } else if (response.event === EventWebhookAsaas.PAYMENT_RECEIVED) {
-      console.log("PAYMENT_RECEIVED", response);
-    }
+      const paymentId: { id: string }[] = await strapi.entityService.findMany(
+        "api::payment.payment",
+        {
+          filters: {
+            transactionId: response.payment.paymentLink,
+          },
+          fields: ["id"],
+        }
+      );
+      if (paymentId.length === 0) {
+        throw new Error("Payment not found");
+      }
 
-    return ctx.request.body;
-  }
-}));
+      await strapi.entityService.update(
+        "api::payment.payment",
+        paymentId[0].id,
+        {
+          data: {
+            status:
+              response.payment.status === "CONFIRMED"
+                ? "paid"
+                : response.payment.status === "PENDING"
+                ? "in process"
+                : "cancelled",
+          },
+        }
+      );
+
+      return ctx.request.body;
+    },
+  })
+);
